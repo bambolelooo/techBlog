a774ad6df4d94a41122ee82d537490158df95c71
Ilya Evlanov <evlanov.ilya@gmail.com>
2023-03-27 12:57:34 -0400

    added delete routes and dashboard page

--- public/js/post.js
+++ public/js/post.js
@@ -14,7 +14,7 @@ const commentFormHandler = async (event) => {
         })
 
         if (response.ok) {
-            // If successful, redirect the browser to the profile page
+            // If successful, redirect the browser to the dashboard page
             document.location.replace(window.location.href)
         } else {
             alert(response.statusText)
