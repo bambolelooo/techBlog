a37c712de262aaf147f86d7060c2e9b5b2491d03
Ilya Evlanov <evlanov.ilya@gmail.com>
2023-03-27 12:15:46 -0400

    added post page and routes

---
+++ public/js/post.js
@@ -0,0 +1,27 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault()
+
+    const comment = document.querySelector('#comment').value.trim()
+    console.log(comment)
+    const url = new URL(window.location.href)
+    const postId = url.pathname.split('/').pop()
+    if (comment) {
+        // Send a POST request to the API endpoint
+        const response = await fetch(`/api/posts/${postId}/comment`, {
+            method: 'POST',
+            body: JSON.stringify({ content: comment }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+
+        if (response.ok) {
+            // If successful, redirect the browser to the profile page
+            document.location.replace(window.location.href)
+        } else {
+            alert(response.statusText)
+        }
+    }
+}
+
+document
+    .querySelector('.comment-form')
+    .addEventListener('submit', commentFormHandler)
